aa7db350cb8142add0cd83b4580a52fdd8f19d1d
smhubble09 <smhubble09@byui.edu>
2019-06-22 21:54:36 -0600

    commit

--- assignments/lesson-9/scripts/currentdate.js
+++ assignments/lesson-9/scripts/currentdate.js
@@ -1,43 +1,16 @@
-function currentDate() [
-var today = '0';
-var day = today.getDay():
-var dd = today.getDate();
-var mm = today.getMonth()+1; 
+var today = new Date();
+var day = new Array(7);
+day[0] = 'Sunday';
+day[1] = 'Monday';
+day[2] = 'Tuesday';
+day[3] = 'Wednesday';
+day[4] = 'Thursday';
+day[5] = 'Friday';
+day[6] = 'Saturday';
+
+var dd = day[today.getDay()];
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+var mm = months[today.getMonth()]; 
 var yyyy = today.getFullYear();
 
-switch (day){
-	case 0:
-		day = 'Sunday';
-		break;
-	case 1:
-		day = 'Monday';
-		break;
-	case 2:
-		day = 'Tuesday';
-		break;
-	case 3:
-		day = 'Wednesday';
-		break;
-	case 4:
-		day = 'Thursday';
-		break;
-	case 5:
-		day = 'Friday';
-		break;
-	case 6:
-		day = 'Saturday';
-}
-
-if(dd < 10) {
-    dd = '0' + dd;
-} 
-
-if(mm < 10) {
-    mm = '0' + mm;
-} 
-today = day + ',' + dd + '' + mm + '' + yyyy;
-
-return today;
-}
-
-document.getElementById('currentDate').innerHTML = currentDate();
+document.getElementById('currentDate').innerHTML = dd + ', ' + mm + ' ' + today.getDate() + ' ' + yyyy;
