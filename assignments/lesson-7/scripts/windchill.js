a8b2f0e6ef10487baa1cf7e92264602b9b053af6
smhubble09 <smhubble09@byui.edu>
2019-07-08 19:26:32 -0600

    commit

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,14 +1,9 @@
-function windChill() {
-
 var t = parseInt(document.getElementById('airTemp').innerHTML);
 
 var s = parseInt(document.getElementById('windSpeed').innerHTML);
 
-var f = 35.74 + (.6215*t) - (35.75*Math.pow(s, .16)) + (.4275*t*pow(s, .16));
+var f = (35.74 + (.6215*t)) - (35.75*Math.pow(s, .16)) + (.4275*t*Math.pow(s, .16));
 
-f = Math.round(f);
+chill = Math.round(f);
 
-return f;
-}
-
-document.getElementById('windChill').innerHTML = windChill();
+document.getElementById('windChill').innerHTML = chill;
